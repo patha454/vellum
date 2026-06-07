27ffb3e414a9736d78d43c5b1a3f9f241779dc36
H Paterson <26947190+patha454@users.noreply.github.com>
2026-06-07 18:42:35 +1000

    Refactor: Move cell styling to `struct VeStyle`

--- ve_cell.h
+++ ve_cell.h
@@ -2,23 +2,18 @@
 #define VELLUM_VE_CELL_H
 
 #include "ve_char.h"
-#include "ve_colour.h"
+#include "ve_style.h"
 
 /**
- * Vellum representation of a single cell of console text.
- *
- * \see ve_char_t
- * \see ve_colour_t
+ * \brief Vellum representation of a single cell of console
+ * text.
  */
 struct VeCell {
     /** Text character in the cell. */
     ve_char_t character;
 
-    /** Foreground colour of the cell. */
-    ve_colour_t foreground;
-
-    /** Background colour of the cell. */
-    ve_colour_t background;
+    /** Appearance of the character in the cell. */
+    struct VeStyle style;
 };
 
 #endif
---
+++ ve_style.h
@@ -0,0 +1,17 @@
+#ifndef VELLUM_VE_STYLE_H
+#define VELLUM_VE_STYLE_H
+
+#include "ve_colour.h"
+
+/**
+ * \breuf Rules dictating cells' (characters') appearance.
+ */
+struct VeStyle {
+    /** Foreground colour of the styled cells. */
+    ve_colour_t foreground;
+
+    /** Background colour of the styled cells. */
+    ve_colour_t background;
+};
+
+#endif // VELLUM_VE_STYLE_H
