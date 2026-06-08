41dc4d111b8417601d1ae66ffd7e09c74ca9637e
H Paterson <26947190+patha454@users.noreply.github.com>
2026-06-08 21:51:37 +1000

    Docs: Fully document `ve_framebuffer.h`

--- ve_framebuffer.h
+++ ve_framebuffer.h
@@ -5,6 +5,21 @@
 #include "ve_cell.h"
 #include <stddef.h>
 
+/**
+ * \brief Vellum framebuffer abstraction.
+ *
+ * Framebuffers contain cells of text characters and their
+ * associated style attributes which will be rendered to a
+ * terminal.
+ *
+ * Vellum framebuffers are polymorphic - members used by
+ * any specific backend are hidden within their individual
+ * implementation.
+ *
+ * \see veFramebufferCreate()
+ * \see veFramebufferDestroy()
+ * \see veFramebufferGetCell()
+ */
 struct VeFramebuffer {
     /**
      * Interface to an implementation of a framebuffer.
@@ -13,16 +28,24 @@ struct VeFramebuffer {
 };
 
 /**
- * \brief Allocate and initialize a new Vellum framebuffer.
+ * \brief Function signature for creating a framebuffer
+ * implementation.
  *
- * Create a mew framebuffer implementation by allocating
+ * Create a new framebuffer implementation by allocating
  * and initializing all required resources.
  *
+ * \param backend Backend implementation to use for the
+ * framebuffer.
  * \param width Width of the new framebuffer, in cells.
  * \param height Height of the new framebuffer, in cells.
  *
  * \note Creating a framebuffer with the width or height
  * set to zero is invalid and will return null.
+ *
+ *  \note This function is part of the abstract framebuffer
+ * module interface. Consumers constructing framebuffers
+ * should use \ref veFramebufferCreate to allocate a new
+ * framebuffer.
  *
  * \return Newly allocated framebuffer, or null if
  * allocation failed.
@@ -32,10 +55,16 @@ typedef struct VeFramebuffer* (*ve_framebuffer_create_t)(
     size_t width, size_t height);
 
 /**
- * \brief Destroy and deallocated a Vellum framebuffer.
+ * \brief Function signature for destroying a framebuffer
+ * implementation.
  *
  * Destroy a framebuffer implementation by freeing and
  * deallocating the resources it holds.
+ *
+ * \note This function is part of the abstract framebuffer
+ * module interface. Consumers destroying framebuffers
+ * should use \ref veFramebufferDestroy to destroy a
+ * framebuffer.
  *
  * \param framebuffer Framebuffer to be deallocated. No
  * action is taken if the framebuffer is null.
@@ -44,7 +73,8 @@ typedef void (*ve_framebuffer_destroy_t)(
     struct VeFramebuffer* framebuffer);
 
 /**
- * \brief Access a framebuffer cell.
+ * \brief Function signature for accessing a cell of a
+ * particular framebuffer implementation.
  *
  * Framebuffer implementations' cells are accessed via
  * zero-indexed coordinates, where the x-axis increases
@@ -52,7 +82,7 @@ typedef void (*ve_framebuffer_destroy_t)(
  * bottom.
  *
  * \note The memory layout of cells within the framebuffer
- * is defined in \ref VeFramebuffer.
+ * is defined by the underlying backend implementation.
  *
  * \param framebuffer Framebuffer to query.
  * \param x X coordinate of the cell.
@@ -66,6 +96,14 @@ typedef struct VeCell* (*ve_framebuffer_get_cell_t)(
     const struct VeFramebuffer* framebuffer, size_t x,
     size_t y);
 
+/**
+ * \brief Implementation of a Vellum framebuffer.
+ *
+ * Virtual method table (vtable) providing the behaviour
+ * for a specific framebuffer implementation. Users select
+ * a framebuffer implementation with the instance passed to
+ * \ref veFramebufferCreate().
+ */
 struct VeFramebufferBackend {
     /**
      * Create a new instance of a framebuffer
@@ -89,6 +127,20 @@ struct VeFramebufferBackend {
 extern const struct VeFramebufferBackend
     veDefaultFramebufferBackend;
 
+/**
+ * \brief Create a new framebuffer implementation by
+ * allocating and initializing all required resources.
+ *
+ * \note Creating a framebuffer with the width or height
+ * set to zero is invalid and will return null.
+ *
+ * \param backend Framebuffer implementation to use.
+ * \param width Width of the new framebuffer, in cells.
+ * \param height Height of the new framebuffer, in cells.
+ *
+ * \return Newly initialized framebuffer, or NULL on
+ * failure.
+ */
 static inline struct VeFramebuffer* veFramebufferCreate(
     const struct VeFramebufferBackend* backend,
     const size_t width, const size_t height)
@@ -99,6 +151,14 @@ static inline struct VeFramebuffer* veFramebufferCreate(
     return backend->create(backend, width, height);
 }
 
+/**
+ * \brief Destroy a framebuffer implementation by freeing
+ * and deallocating all resources it holds.
+ *
+ * \note No action will be taken if the framebuffer is NULL.
+ *
+ * \param framebuffer Framebuffer to destroy.
+ */
 static inline void veFramebufferDestroy(
     struct VeFramebuffer* framebuffer)
 {
@@ -109,6 +169,22 @@ static inline void veFramebufferDestroy(
     framebuffer->backend->destroy(framebuffer);
 }
 
+/**
+ * \brief Access a single cell within a framebuffer.
+ *
+ * Framebuffer implementations' cells are accessed via
+ * zero-indexed coordinates, where the x-axis increases
+ * from left to right and the y-axis increases from top to
+ * bottom.
+ *
+ * \param framebuffer Framebuffer to query.
+ * \param x X coordinate of the cell.
+ * \param y Y coordinate of the cell
+ *
+ * \return Cell at (x, y) in the framebuffer, or null if:
+ *   - The coordinates are out of range.
+ *   - The framebuffer is null or invalid.
+ */
 static inline struct VeCell* veFramebufferGetCell(
     const struct VeFramebuffer* framebuffer, size_t x,
     size_t y)
